642d0343166d9dba4f63b841ef2e4c9e70c56991
Lucas pereira <lucasgunner3@gmail.com>
2025-01-28 20:48:06 -0300

    refact: melhorando estrutura

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import appointmentController from '../controllers/appointmentController.js';
+import doctorController from '../controllers/doctorController.js';
+import pacientController from '../controllers/pacientController.js';
+import prescriptionController from '../controllers/prescriptionController.js';
 
 const router = express.Router();
 
@@ -6,5 +10,10 @@ router.get('/', (req, res) => {
     console.info('GET /');
     res.status(200).json({ message: 'Hello, world!' });
 });
+
+router.use("/", appointmentController);
+router.use("/", doctorController);
+router.use("/", pacientController);
+router.use("/", prescriptionController);
 
 export default router;
